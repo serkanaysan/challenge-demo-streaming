53acb791da2f9e2d89e7503715d60bf1729770cb
Serkan Aysan <serkan@themediamove.com>
2018-06-01 00:21:02 +0300

    challenge

--- server.js
+++ server.js
@@ -5,8 +5,4 @@ const port = process.env.PORT || 5000;
 
 app.use('/api', require('./routes/api'));
 
-app.get('/api/hello', (req, res) => {
-  res.json({ express: 'Hello From Express' });
-});
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
